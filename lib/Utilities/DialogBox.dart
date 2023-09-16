1d4472d91ba3b25bc57bba397b9e2022a1919ea2
Karlo11111 <globalranking12@gmail.com>
2023-09-16 23:45:52 +0200

    Create DialogBox.dart

---
+++ lib/Utilities/DialogBox.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DialogBoxWidget extends StatefulWidget {
+  const DialogBoxWidget({super.key});
+
+  @override
+  State<DialogBoxWidget> createState() => _DialogBoxWidgetState();
+}
+
+class _DialogBoxWidgetState extends State<DialogBoxWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
