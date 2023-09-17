63faa44b7f48c8686a31e46703a72ceaaf522c2a
Karlo11111 <globalranking12@gmail.com>
2023-09-17 14:04:50 +0200

    Update ToDoTile.dart

--- lib/Utilities/ToDoTile.dart
+++ lib/Utilities/ToDoTile.dart
@@ -2,6 +2,7 @@
 // ignore: file_names
 // ignore_for_file: file_names
 
+
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
@@ -19,22 +20,32 @@ class ToDoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.only(right: 25, left: 25, top: 25),
+
+      
+      return Padding(
+      padding: const EdgeInsets.only(right: 25, left: 25, top: 25, bottom: 12.5),
         child: Container(
           padding: const EdgeInsets.all(20),
           decoration: BoxDecoration(color: Colors.blue[200]),
+          
           child: Row(
             children: [
               //checkbox
               Checkbox(value: taskCompleted, onChanged: onChanged,
-              
+              checkColor: Colors.black,
               ),
               //task name
-              Text(taskName),
+              Text(
+                taskName, 
+                style: TextStyle(decoration: taskCompleted? TextDecoration.lineThrough : TextDecoration.none, fontSize: 35),
+              ),
             ],
           ),
           )
-       );
+       ); 
+   
   }
+
+
+ 
 }
